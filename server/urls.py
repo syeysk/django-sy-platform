20587336464b2989bcc94f339208aec0272d543f
shyzik93 <shyzik93@mail.ru>
2023-11-19 03:30:24 +0300

    add token settings

--- server/urls.py
+++ server/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('server.urls_api')),
     path('auth/', include('django_sy_framework.custom_auth.urls')),
+    path('token/', include('django_sy_framework.token.urls')),
     path('project/', include('project.urls')),
     path('', include('django_sy_framework.base.urls')),
 ]
